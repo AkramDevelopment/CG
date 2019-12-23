317c0c853be5de84ffe02e1f0d1533d513745e2d
Akram <akram.development50@gmail.com>
2019-12-23 15:04:58 -0600

    Added Session Experitation

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,8 @@
-from flask import Blueprint,Flask
+from datetime import timedelta
+from flask import Blueprint,Flask,session
 from backend.users.view import user_blueprint
 from backend.announcements.view import announcements_blueprint
+from backend.auth.view import auth_blueprint
 
 
 app = Flask(__name__)
@@ -8,5 +10,7 @@ app = Flask(__name__)
 
 
 app.config["SECRET_KEY"] = "PLACEHOLDER"
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=60)
 app.register_blueprint(user_blueprint,url_prefix="/user")
 app.register_blueprint(announcements_blueprint,url_prefix="/announcements")
+app.register_blueprint(auth_blueprint,url_prefix="/auth")
