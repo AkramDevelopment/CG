dba145765d480d943f04f79c1986d79e30bd77a2
Akram <akram.development50@gmail.com>
2020-01-09 01:28:47 -0600

    frontend_playground

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="app-footer">
+        
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style scoped>
+footer.app-footer {
+    width: 100%;
+    height: 80px;
+    position:fixed;
+    left: 0;
+    bottom: 0;
+    margin: 0;
+    background-color: var(--primary-color);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+footer.app-footer h1 {
+    font-size: 2.5rem;
+    text-transform: uppercase;
+}
+</style>
