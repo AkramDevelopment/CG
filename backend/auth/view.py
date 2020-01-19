fade324ffb918153552677ef44434cfc7d97297d
Akram <akram.development50@gmail.com>
2020-01-18 18:03:34 -0600

    Update view.py

--- backend/auth/view.py
+++ backend/auth/view.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,Flask,render_template,session,request,jsonify,make_response,session
-from werkzeug.security import check_password_hash
 from backend.db.Accounts import Query_Account_By_Email,Add_User,Query_All_Accounts,Add_Admin
 from backend.users.auth import adminRequired,login_required,Developer_Required
+from werkzeug.security import check_password_hash
 from functools import wraps
 from jwt import encode,decode
 
