9d437b6d7f44710acf4d597d664769cb3e8b5507
Akram <akram.development50@gmail.com>
2020-01-18 19:39:19 -0600

    Add global fields file

---
+++ backend/config/fields.py
@@ -0,0 +1,23 @@
+Account_Fields =  { 
+    "first-name" : "first-name",
+    "last-name" : "last-name",
+    "password" : "password",
+    "email" : "password"
+
+}
+
+Event_Fields = { 
+    "event-title" : 'event-title',
+    "date-start" : 'date-start',
+    'date-end' : 'date-end',
+    'time-start' : 'time-start',
+    'time-end' :'time-end',
+    'location' : 'location'
+}
+
+Banned_Account_Fields = { 
+    "account-id" : 'account-id',
+    "banned-by" : 'banned-by'
+}
+
+
