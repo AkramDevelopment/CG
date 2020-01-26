d0810eefbc90b72a901466a08644ee08d06d3f33
Akram <akram.development50@gmail.com>
2020-01-25 20:15:52 -0600

    Got Rid Of Secret Key File.
    Made the secret key re-generate everytime a new instance of the server is ran.

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-from backend.config.secret_key import key
 from flask import Blueprint,Flask,session
 from backend.users.view import user_blueprint
 from backend.announcements.view import announcements_blueprint
@@ -7,13 +6,14 @@ from backend.auth.registration import registeration_blueprint
 from backend.auth.view import auth_blueprint
 from backend.events.view import event_blueprint
 from backend.groups.view import groups_blueprint
+import os
 
 
 app = Flask(__name__)
 
 
 
-app.config["SECRET_KEY"] = key['secret_key']
+app.config["SECRET_KEY"] = os.urandom(80)
 app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=60)
 app.register_blueprint(user_blueprint,url_prefix="/user")
 app.register_blueprint(announcements_blueprint,url_prefix="/announcements")
@@ -21,3 +21,6 @@ app.register_blueprint(auth_blueprint,url_prefix="/auth")
 app.register_blueprint(registeration_blueprint,url_prefix="/auth")
 app.register_blueprint(event_blueprint,url_prefix="/events")
 app.register_blueprint(groups_blueprint,url_prefix="/group")
+
+
+
