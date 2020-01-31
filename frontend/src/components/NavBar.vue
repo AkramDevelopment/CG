19936eec318bf6691e634f563ca137217e7c8850
Akram <akram.development50@gmail.com>
2020-01-31 14:56:17 -0600

    Update Navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="navbar-wrapper">
     <div class="sidenav">
-      <a href="#">Home</a>
-      <b-dropdown class="navbar_dropdown" text=" Admin " v-if="isAdmin" href="#">
+       <a href="#">Home</a>
+        <b-dropdown class="navbar_dropdown" text="Admin" v-if="isAdmin" href="#">
         <b-dropdown-item>Announcements</b-dropdown-item>
         <b-dropdown-item> Meeting Notes </b-dropdown-item>
         <b-dropdown-item>Create Events</b-dropdown-item>
         <b-dropdown-item>Account Applications</b-dropdown-item>
         <b-dropdown-item>Emails</b-dropdown-item>
       </b-dropdown>
-      <a href="#">Meeting Minutes</a>
-      <a href="#">Contact</a>
+      <a href="#">Meeting<br/>Notes</a>
+     
     </div>
 
     <!-- Page content -->
